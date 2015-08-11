bcc48f0a46f58e1182309f6181777d2f6ddd45b0
lily healey <lily@lilyhealey.co.uk>
2015-08-11 18:44:47 -0400

    Initial Commit

---
+++ multi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  multi
+//
+//  Created by Lily Healey on 11.08.2015.
+//  Copyright (c) 2015 O-R-G, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ multi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  multi
+//
+//  Created by Lily Healey on 11.08.2015.
+//  Copyright (c) 2015 O-R-G, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
