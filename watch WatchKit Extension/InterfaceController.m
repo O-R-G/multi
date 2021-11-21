441ce11a9db066516736173b0703fb7ab371dc40
david reinfurt <reinfurt@o-r-g.com>
2021-11-21 09:56:39 -0500

    add watch target

---
+++ watch WatchKit Extension/InterfaceController.m
@@ -0,0 +1,34 @@
+//
+//  InterfaceController.m
+//  watch WatchKit Extension
+//
+//  Created by david reinfurt on 11/21/21.
+//  Copyright © 2021 O-R-G, Inc. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController ()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+}
+
+@end
+
+
+
---
+++ watch WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  watch WatchKit Extension
+//
+//  Created by david reinfurt on 11/21/21.
+//  Copyright © 2021 O-R-G, Inc. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
